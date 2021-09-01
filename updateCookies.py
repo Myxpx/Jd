902ed9d7e8082be38683c70750c87061471f0b11
Myxpx <87304369+Myxpx@users.noreply.github.com>
2021-09-02 00:02:50 +0800

    Add files via upload

--- updateCookies.py
+++ updateCookies.py
@@ -46,97 +46,53 @@ def getckitem(key):
         if key in i["value"]:
             return i
     return []
+
+
 def wstopt(cookies):
-
     headers = {
-
         'user-agent': 'okhttp/3.12.1;jdmall;android;version/10.1.2;build/89743;screen/1080x2293;os/11;network/wifi;',
-
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-
         'Cookie': cookies,
-
     }
-
     url = 'https://api.m.jd.com/client.action?functionId=genToken&clientVersion=10.1.2&build=89743&client=android' \
-
           '&d_brand=&d_model=&osVersion=&screen=&partner=&oaid=&openudid=a27b83d3d1dba1cc&eid=&sdkVersion=30&lang' \
-
           '=zh_CN&uuid=a27b83d3d1dba1cc&aid=a27b83d3d1dba1cc&area=19_1601_36953_50397&networkType=wifi&wifiBssid=&uts' \
-
           '=&uemps=0-2&harmonyOs=0&st=1630413012009&sign=ca712dabc123eadd584ce93f63e00207&sv=121'
-
     body = 'body=%7B%22to%22%3A%22https%253a%252f%252fplogin.m.jd.com%252fjd-mlogin%252fstatic%252fhtml' \
-
            '%252fappjmp_blank.html%22%7D&'
-
     response = requests.post(url, data=body, headers=headers, verify=False)
-
     data = json.loads(response.text)
-
     if data.get('code') != '0':
-
         return None
-
     tokenKey = data.get('tokenKey')
-
     url = data.get('url')
-
     session = requests.session()
-
     params = {
-
         'tokenKey': tokenKey,
-
         'to': 'https://plogin.m.jd.com/jd-mlogin/static/html/appjmp_blank.html'
-
     }
-
     url += '?' + urlencode(params)
-
     session.get(url, allow_redirects=True)
-
     result = ""
-
     for k, v in session.cookies.items():
-
         if k == 'pt_key' or k == 'pt_pin':
-
             result += k + "=" + v + "; "
-
     return result
 
 
-
-
-
 def update(text, qlid):
-
     url = "http://127.0.0.1:5700/api/envs?t=%s" % gettimestamp()
-
     s.headers.update({"Content-Type": "application/json;charset=UTF-8"})
-
     data = {
-
         "name": "JD_COOKIE",
-
         "value": text,
-
         "_id": qlid
-
     }
-
     r = s.put(url, data=json.dumps(data))
-
     if json.loads(r.text)["code"] == 200:
-
         return True
-
     else:
-
         return False
-
-
 
 
 def insert(text):
